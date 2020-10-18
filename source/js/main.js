1a9774b363a715c5d463218878f395bc890b2fc1
Sokres <fieldytr@gmail.com>
2020-10-18 16:38:54 +0300

    Почти закнечено меню, осталось раобаться с индексом :activ

--- source/js/main.js
+++ source/js/main.js
@@ -1,1 +1,27 @@
-console.log("hello, world");
+var menuLvl = document.querySelector('.site-list__item--down');
+var menuLvlClick = document.querySelector('.site-list__link-subdmenu');
+var navMain = document.querySelector('.bd');
+var navToggle = document.querySelector('.main-nav__btn');
+/*Menu*/
+menuLvlClick.addEventListener('click', function() {
+  if (menuLvl.classList.contains('site-list__item--down-close')) {
+  menuLvl.classList.remove('site-list__item--down-close');
+  menuLvl.classList.add('site-list__item--down-open');
+} else {
+  menuLvl.classList.add('site-list__item--down-close');
+  menuLvl.classList.remove('site-list__item--down-open');
+}
+});
+
+navMain.classList.remove('header__nav--nojs');
+
+navToggle.addEventListener('click', function() {
+  if (navMain.classList.contains('close')) {
+    navMain.classList.remove('close');
+    navMain.classList.add('open');
+  } else {
+    navMain.classList.add('close');
+    navMain.classList.remove('open');
+  }
+});
+console.log('helo');
