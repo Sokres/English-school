31c0dc55f1d9ac285fe8740de37bcc82799e54c8
Sokres <fieldytr@gmail.com>
2021-01-18 23:05:31 +0300

    переезд на Github

--- source/js/main.js
+++ source/js/main.js
@@ -28,7 +28,7 @@ console.log('helo');
 
 
 //Скрипт для программы курсов
-
+(function(){
   var accordeonProgramm = document.getElementsByClassName("programm__btn");
   var i;
 
@@ -43,11 +43,12 @@ console.log('helo');
       }
     });
   }
+})();
 
 //Модальное окно для теста определи свой уровень
 var modalLvl = document.getElementById("lvlModal");
 
-var openBtnLvl = document.getElementById("lvlOpenModal");
+var openBtnLvl = document.querySelector(".btn-test, .test");
 
 var closeLvl = document.getElementsByClassName("your-lvl__close")[0];
 
@@ -412,3 +413,45 @@ function Click(index)
 	//Ждём секунду и обновляем тест
 	setTimeout(Update, 1000);
 }
+
+
+
+
+//Липкое меню//
+
+(function() {
+  'use strict';
+
+  var section = document.querySelectorAll(".section-menu");
+  var sections = {};
+  var i = 0;
+
+  Array.prototype.forEach.call(section, function(e) {
+    sections[e.id] = e.offsetTop;
+  });
+
+  window.onscroll = function() { myFunction();
+    var scrollPosition = document.documentElement.scrollTop || document.body.scrollTop;
+
+    for (i in sections) {
+      if (sections[i] <= scrollPosition) {
+        document.querySelector('.welcome-info__submenu--active').setAttribute('class', 'welcome-info__link');
+        document.querySelector('.welcome-info__link[href*=' + i + ']').setAttribute('class', 'welcome-info__submenu--active');
+      }
+    }
+  };
+  // window.onscroll = function() {myFunction()};
+  var navbar = document.getElementById("stickSubmenu");
+  var wrapnav = document.getElementById("stick");
+  var sticky = navbar.offsetTop;
+  function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("welcome-info__submenu--stick")
+    wrapnav.classList.add("menu-stick")
+
+  } else {
+    navbar.classList.remove("welcome-info__submenu--stick")
+    navbar.classList.remove("menu-stick");
+  }
+}
+})();
